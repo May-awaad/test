f972e16415b35897a11c36a4027384ddecb450fb
May-Abdelgany <mayawaad622@gmail.com>
2023-11-16 18:54:45 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
+import cors from 'cors'
 import express from "express";
 import bootstrap from "./src/index.router.js";
 const app = express();
+app.use(cors())
 const port = process.env.PORT || 3000;
-app.get("/", (req, res, next) => {
-    return res.json({ message: "In-valid Routing" })
-});
 
-// bootstrap(app, express);
+
+bootstrap(app, express);
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- src/index.router.js
+++ src/index.router.js
@@ -2,10 +2,9 @@
 import connectDB from '../DB/connection.js'
 import authRouter from './modules/auth/auth.router.js'
 import userRouter from './modules/user/user.router.js'
-import cors from 'cors'
+
 import { globalErrorHandling } from './utils/errorHandling.js'
 const bootstrap = (app, express) => {
-    app.use(cors())
     app.use(express.json())
     app.use("/auth", authRouter)
     app.use("/user", userRouter)
